e80b859de6bc63c09c20612332a08b43a8c9203d
gift56 <efeasiughu@gmail.com>
2022-06-03 13:20:22 +0100

    done with the js coding

--- styles/main.js
+++ styles/main.js
@@ -1,8 +1,14 @@
 const activeChange = document.querySelectorAll('ul li');
 
+const removeActive = () => {
+    activeChange.forEach(item => {
+        item.classList.remove('active');
+    })
+}
 
 activeChange.forEach(item => {
     item.addEventListener('click', () => {
+        removeActive();
         item.classList.add('active');
     })
 })
